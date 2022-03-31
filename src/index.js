be3737ccf43d9e85adbc727a7e1df58f6fbec04f
Keli Booster <kbooster17@gmail.com>
2022-03-31 21:05:56 +0000

    add patch routes

--- src/index.js
+++ src/index.js
@@ -15,67 +15,119 @@ app.post("/v1/users", async (req, res) => {
     await user.save();
     res.status(201).send(user);
   } catch (e) {
-    res.status(400).send("error");
+    res.status(400).send(e);
   }
 });
 
-app.post("/v1/tasks", (req, res) => {
+app.post("/v1/tasks", async (req, res) => {
   const task = Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get("/v1/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+app.get("/v1/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.get("/v1/users/:id", (req, res) => {
-  const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send("User was not found!");
-      }
-      res.send(user);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
-});
-
-app.get("/v1/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
-});
-
-app.get("/v1/tasks/:id", (req, res) => {
+app.get("/v1/users/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then((task) => {
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("User was not found!");
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.get("/v1/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/v1/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    res.send(task);
+  } catch {
+    res.status(400).send(e);
+  }
+});
+
+app.patch("/v1/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updates = Object.keys(req.body);
+  const allUpdates = ["name", "email", "password", "age"];
+  const isValidOp = updates.every((update) => allUpdates.includes(update));
+
+  if (!isValidOp) {
+    return res
+      .status(400)
+      .send(
+        "Invalid Operation: You are most likely trying to update a field we do not have in the db."
+      );
+  }
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
     });
+    if (!user) {
+      return res.status(404).send("User was not found!");
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.patch("/v1/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updates = Object.keys(req.body);
+  const allUpdates = ["description", "completed"];
+  const isValidOp = updates.every((update) => allUpdates.includes(update));
+
+  console.log(updates);
+
+  if (!isValidOp) {
+    return res
+      .status(400)
+      .send(
+        "Invalid Operation: You are most likely trying to update a field we do not have in the db."
+      );
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send("Task was not found!");
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
